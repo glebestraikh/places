6dea60b27a21f3ab980eccee472d173eeb6d5f2a
Gleb Gleb <g.estraikh@gmail.com>
2025-10-25 11:34:29 +0700

    set GOMAXPROCS to 1 for asynchronous operation of goroutines

--- cmd/places/main.go
+++ cmd/places/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
-
 	"places/internal/app"
+	"runtime"
 )
 
 func main() {
+	runtime.GOMAXPROCS(1) // горутины асинхронны, но не параллельны
+
 	application := app.NewApp()
 
 	if err := application.Run(":8080"); err != nil {
